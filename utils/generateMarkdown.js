cb6b97a9fa365d44b256565bec623422b466c25b
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-07-22 17:37:43 -0500

    added markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,23 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description\n${data.description}
+  ## Table of Contents
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Credits](#credits)
+  -[License](#license)
+  -[How To Contribute](#contributions)
+  -[Tests](#testing)
+  ## Installation\n\n${data.installation}\n
+  ## Usage\n\n${data.usage}\n
+  ## Credits\n\n${data.credits}\n
+  ## License\n\nThis project is licensed under ${data.license}.
+  ## How To Contribute\n\n${data.contributions}\n
+  ## Tests\n\n${data.testing}
+  ## Question\n If you have any questions, please reach out to my github account: [Github](https://github.com/${data.github}), or my email: [${info.contact}](${info.contact})
 `;
 }
+
 
 module.exports = generateMarkdown;
