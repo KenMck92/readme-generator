0e27853a502a8aa53a5df508083b1a158b05dd13
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-07-22 17:01:37 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markDown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
+function questions() {
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your title?',
+            name: 'Title',
+        },
+        {
+            type: 'input',
+            message: 'What is the description of your Readme?',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'Table of Contents?',
+            name: 'Table of Contents',
+        },
+        {
+            type: 'input',
+            message: 'Installation',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'Usage',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'License',
+            name: 'License',
+        },
+        {
+            type: 'input',
+            message: 'Contributing',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message: 'Test',
+            name: 'Tests',
+        },
+        {
+            type: 'input',
+            message: 'Questions',
+            name: 'Questions',
+        },
+    ])
+    .then((response) => 
+    writeToFile(markDown(response))
+    );
+};
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('README.md',`${data}\n`, (err) =>
+        err ? console.log(err): console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
